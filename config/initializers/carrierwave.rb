e8c01823792588a94ecd1872c81c6eeaa2312110
Boonyakorn Wasusopon <trust_wasu@hotmail.com>
2017-03-30 01:40:23 -0400

    edit ENV

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,5 +5,5 @@ CarrierWave.configure do |config|
     :aws_access_key_id     => ENV["aws_access_key_id"],
     :aws_secret_access_key => ENV["aws_secret_access_key"],
   }
-  config.fog_directory  => ENV["fog_directory"]
+  config.fog_directory  = ENV["fog_directory"]
 end
