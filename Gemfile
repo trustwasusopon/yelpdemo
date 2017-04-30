43d8228dc00b132c9ff6f189c72bffa5f986ac15
Boonyakorn Wasusopon <trust_wasu@hotmail.com>
2017-04-30 00:16:14 -0400

    add user accounts with Devise

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'figaro'
 gem 'mini_magick'
 gem 'fog-aws'
 gem 'carrierwave-aws'
+gem 'devise'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,4 +85,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #required for Devise, remember to change localhost:3000 to actual application host.
+  config.action_mailer.default_url_options = { host: 'www.yelpdemobytrust.com'}
+
 end
