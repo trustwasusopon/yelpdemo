cb95ef53f85c9466f50b75c10bc2f606ba8384ff
Boonyakorn Wasusopon <trust_wasu@hotmail.com>
2017-05-02 01:51:49 -0400

    Add Data Validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,4 +4,15 @@ class Restaurant < ApplicationRecord
 
   has_many :reviews
 
+  validates :name, :address, :phone, :website, :image, presence: true
+
+  validates :phone, format: { with: /\A\(\d{3}\) \d{3}-\d{4}\z/,
+      message: "must be in the format (123) 456-7890" }
+
+  validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
+      message: "must start with http:// or https://" }
+
+  validates :address, format: { with: /\A\d+[^,]+,[^,]+, [A-Z]{2} \d{5}\z/,
+      message: "must be in the format of 350 Fifth Avenue, New York, NY 10118" }
+
 end
